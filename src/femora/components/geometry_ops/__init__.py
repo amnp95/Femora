0495cea5fa2349a0b7172e43876e632f96d0f9ec
amnp95 <92498292+amnp95@users.noreply.github.com>
2026-01-22 00:06:51 +0000

    docs: standardize docstrings in src/femora/components/geometry_ops/__init__.py

--- src/femora/components/geometry_ops/__init__.py
+++ src/femora/components/geometry_ops/__init__.py
@@ -2,5 +2,3 @@ from .spatial_transform_manager import SpatialTransformManager
 from .meshpart_transform_proxy import MeshPartTransform
 
 __all__ = ["SpatialTransformManager", "MeshPartTransform"]
-
-
